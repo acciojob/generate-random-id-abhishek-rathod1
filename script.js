f1d77067f5cd40d4e9a23e809f13f8b2921199a4
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-13 18:01:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function makeid(l) {
   // write your code here
+	const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	let result = ""
+	let charactersLength = characters.length;
+	let counter = 0;
+	while (counter<l) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		counter++;
+	}
+	return result;
 }
 
-// Do not change the code below.
+// Do not change the code below.d
 const l = prompt("Enter a number.");
 alert(makeid(l));
